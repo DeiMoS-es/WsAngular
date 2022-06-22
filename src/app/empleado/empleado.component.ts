ba5768c37ad0e03854b42dff945e53751eb8a557
DeiMoS <nagib.86@gmail.com>
2022-06-22 16:57:57 +0200

    template y styles inline

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
-  templateUrl: './empleado.component.html',
+  templateUrl: './empleado.component.html', 
+  //template: "<p>Aqui iría un empleado</p>", template inline
   styleUrls: ['./empleado.component.css']
+  //styles: ["p{background-color:red}"]  styles inline
 })
 export class EmpleadoComponent implements OnInit {
 
