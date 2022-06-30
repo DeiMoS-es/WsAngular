22d1632a80aacb0b40ae4da8e3e7340452dcc8b3
DeiMoS <nagib.86@gmail.com>
2022-06-30 12:23:02 +0200

    calculadora creada

--- miniCalculadoraApp/src/app/app.component.ts
+++ miniCalculadoraApp/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalculadoraApp';
+  title = 'Mini Calculadora';
+  // creo variables de clase
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  //creo metodos
+  sumar():void{
+    //con this, especifico que resultado corresponde a esta clase
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  restar():void{
+    //con this, especifico que resultado corresponde a esta clase
+    this.resultado = this.numero1 - this.numero2;
+  }
 }
