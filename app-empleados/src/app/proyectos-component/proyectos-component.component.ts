82f722ee4c3a974c9b39697c39be602d1a3f4cc3
DeiMoS <nagib.86@gmail.com>
2022-07-09 10:56:04 +0200

    formulario más botón volver

--- app-empleados/src/app/proyectos-component/proyectos-component.component.ts
+++ app-empleados/src/app/proyectos-component/proyectos-component.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-proyectos-component',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyectos-component.component.css']
 })
 export class ProyectosComponentComponent implements OnInit {
-
-  constructor() { }
-
+  titulo = 'Listado de Empleados';
+  cuadroNombre: String = "";
+  cuadroApellido: String = "";
+  cuadroCargo: String = "";
+  cuadroSalario: number = 0;
+  constructor(private router: Router, private miServicio: ServicioEmpleadosService, private empleadosService: EmpleadosService) { }
+ 
+  empleados: Empleado[] = [] //debemos dejar un array vacio para que cuando inyete la info del servicio se almacene en este array
   ngOnInit(): void {
+    this.empleados = this.empleadosService.empleados;
   }
-
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    //Antes de añadir el empleado, quiero hacer uso del servicio que acabamos de crear
+   // this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre); no la neesito porque ya lo he inyectado en el empleados.service.ts
+    //this.empleados.push(miEmpleado); lo uso tambien en el servicio
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);//en este servicio es donde está todo lo anterior
+    this.router.navigate([""]);//redireccionamiento automatico después de hacer una acción
+  }
+  public volverHome(){
+    this.router.navigate([""]);//cadena vacía, ya que es la ruta para ir al Home
+  }
 }
